2fc9ef56931c0100cce00b4fd9ba89952e9fb2be
Brittany Barrow <bangelabarrow@gmail.com>
2018-03-28 17:15:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(Ralph) {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(Bob) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(Broom) {
+  var moreKittens = [...kittens, "Broom"]
+  return moreKittens;
+}
+
+function prependKitten(Arnold) {
+  var moreKittens = ["Arnold", ...kittens]
+  return moreKittens;
+}
